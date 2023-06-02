55cf1e6fbc87cc8be5fd15630dd70f3c7da828a6
gygwak <gygwak@surromind.ai>
2023-06-02 18:45:41 +0900

    Feat: 업로드 기능
    업로드 기능을 추가했습니다

--- server/server.py
+++ server/server.py
@@ -6,7 +6,7 @@ import socketio
 
 from logger import Logger
 
-sio = socketio.Server(async_mode=None)
+sio = socketio.Server(async_mode=None, cors_allowed_origins='*')
 
 app = socketio.WSGIApp(sio)
 
@@ -16,21 +16,23 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 logger = Logger().logger
 
-file_status = {}
+download_status = {}
+upload_status = {}
 
 
 @sio.event
-def connect(sid, environ):
+def connect(sid, data, session):
     message = f'client {sid} connected'
     logger.debug('connect: ' + message)
     sio.emit('message_all', {'NOTICE': message})
 
 
 @sio.event
-def disconnect(sid, data, environ):
+def disconnect_request(sid):
     message = f'client {sid} disconnected'
     logger.debug('disconnect: ' + message)
     sio.emit('message_all', {'NOTICE': message})
+    sio.disconnect(sid)
 
 
 @sio.event
@@ -57,18 +59,18 @@ def file_download(sid, data):
         sio.emit('message', {'NOTICE': 'No file with input name'})
 
     # 클라이언트의 sid, file 명으로 chunk를 만들어줄 generator를 구분합니다
-    if sid not in file_status:
-        file_status[sid] = {}
+    if sid not in download_status:
+        download_status[sid] = {}
 
-    if file_name not in file_status[sid]:
+    if file_name not in download_status[sid]:
         chunk_size = data.get('chunk_size', 1024)
-        file_status[sid][file_name] = _file_chunk(sid, file_name, chunk_size)
+        download_status[sid][file_name] = _file_download_chunk(sid, file_name, chunk_size)
 
     # 제너레이터가 출력한 값을 반환합니다
-    sio.emit('file', next(file_status[sid][file_name]), room=sid)
+    sio.emit('file_download', next(download_status[sid][file_name]), room=sid)
 
 
-def _file_chunk(sid, file_name, chunk_size):
+def _file_download_chunk(sid, file_name, chunk_size):
     """
     file chunk를 출력하는 generator 함수입니다
     :param sid: 유저의 sid
@@ -77,16 +79,48 @@ def _file_chunk(sid, file_name, chunk_size):
     :return: 파일을 읽으면서 chunk를 출력하고 전부 읽었다면 종료 메시지를 반환합니다
     """
     # 주어진 파일명의 파일을 엽니다
-    with open(f'{MEDIA_DIR}/{file_name}', 'rb') as f:
+    with open(f'{MEDIA_DIR}\\{file_name}', 'rb') as f:
         # chunk 크기 만큼 파일을 읽어오는 것을 반복합니다
         while file_content := f.read(chunk_size):
             # 읽어온 chunk를 반환합니다
             yield {'content': file_content}
     # 파일을 전부 읽었다면 dict에서 제거합니다
-    del file_status[sid][file_name]
+    del download_status[sid][file_name]
     # 종료 메시지를 보냅니다
     yield {'Message': 'file_end'}
 
 
+@sio.event
+def file_upload(sid, data):
+    file_name = data.get('file_name', None)
+    if file_name is None:
+        logger.warning(f'{sid}: wrong file name - {file_name}')
+        sio.emit('message', {'NOTICE': 'No file with input name'})
+
+    if sid not in upload_status:
+        total = data.get('total', 0)
+        upload_status[sid] = {file_name: {'chunk_number': list(range(total)), 'total': total}}
+
+    waiting_chunk_number = upload_status[sid][file_name]['chunk_number']
+    file_dir = f'{MEDIA_DIR}\\upload\\{file_name}'
+    total_chunk = upload_status[sid][file_name]['total']
+
+    if cursor := data.get('cursor', 0) in waiting_chunk_number:
+        with open(f'{file_dir}_{cursor}', 'wb') as f:
+            f.write(data.chunk)
+            waiting_chunk_number.remove(cursor)
+
+    if not waiting_chunk_number:
+        with open(file_dir, 'wb') as mrg:
+            for i in range(total_chunk):
+                file_chunk_dir = f'{file_dir}_{i}'
+                with open(file_chunk_dir, 'rb') as cnk:
+                    mrg.write(cnk.read())
+
+    progress = len(waiting_chunk_number) / total_chunk
+
+    sio.emit('file_upload', {'Message': f'Upload progress : {progress:2f}%'}, room=sid)
+
+
 if __name__ == '__main__':
-    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+    eventlet.wsgi.server(eventlet.listen(('', 8005)), app)
